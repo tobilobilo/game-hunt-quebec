38d55900f96585fe2b0826935968dbad6cf53008
JP Rose <test@test.com>
2024-11-11 23:36:42 -0500

    redone project structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import "../assets/styles/App.css";
+import SimpleMap from "../features/map/components/SimpleMap";
 
 function App() {
   return (
     <>
       <div>
         <h1>Game Chasing Québec</h1>
+        <SimpleMap />
       </div>
     </>
   );
---
+++ src/features/map/components/SimpleMap.tsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { type LatLngExpression } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const SimpleMap = () => {
+  const mapRef = useRef(null);
+  const latitude = 45.508888; // mtl
+  const longitude = -73.561668; // mtl
+  const position: LatLngExpression = [45.5520502, -73.5513515];
+
+  return (
+    // Make sure you set the height and width of the map container otherwise the map won't show
+    <div id="map">
+      <MapContainer center={[latitude, longitude]} zoom={11} ref={mapRef} style={{ height: "100%", width: "100%" }}>
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* Additional map layers or components can be added here */}
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default SimpleMap;
