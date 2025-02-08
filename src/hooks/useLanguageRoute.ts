6e54416b869d99f25449e65bcaae4e61d54cc1c2
Jean-Pierre Rose <big_moman@hotmail.com>
2025-02-07 21:48:31 -0500

    test env

--- src/hooks/useLanguageRoute.ts
+++ src/hooks/useLanguageRoute.ts
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
 
 const useLanguageRoute = () => {
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
     const params = useParams();
     const { t, i18n } = useTranslation();
 
