befdc57fe92d5562f0b8d51d6a47f989481f5feb
Jean-Pierre Rose <big_moman@hotmail.com>
2025-02-07 22:14:30 -0500

    setup config paths

--- src/hooks/useLanguageRoute.ts
+++ src/hooks/useLanguageRoute.ts
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 const useLanguageRoute = () => {
-    //const navigate = useNavigate();
+    const navigate = useNavigate();
     const params = useParams();
     const { t, i18n } = useTranslation();
 
     useEffect(() => {
         if (!params.lang || (Array.isArray(i18n.options.supportedLngs) && !i18n.options.supportedLngs.includes(params.lang))) {
-            //navigate(`/${i18n.options.fallbackLng}`); TODO
+            navigate(`/${i18n.options.fallbackLng}`);
             return;
         }
         
