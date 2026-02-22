fddbbeeef790c56765c66a0ad0060f152a2e2309
Jean-Pierre Rose <big_moman@hotmail.com>
2026-02-21 22:07:35 -0500

    fix: prettier et config

--- apps/web/src/i18n.js
+++ apps/web/src/i18n.js
@@ -1,7 +1,7 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { basename } from "./utils/path.ts";
 import { SUPPORTED_LOCALES, FALLBACK_LOCALE } from "./config/global.ts";
 
@@ -17,7 +17,7 @@ i18n
       escapeValue: false, // not needed for React
     },
     backend: {
-      loadPath: basename().concat('/locales/{{lng}}.json'), // path to the translation files
+      loadPath: basename().concat("/locales/{{lng}}.json"), // path to the translation files
     },
   });
 
