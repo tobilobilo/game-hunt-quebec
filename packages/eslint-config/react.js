04cf15e8d693fdab6f1c7acd70fda6c0ad557db2
Jean-Pierre Rose <big_moman@hotmail.com>
2026-03-04 23:26:02 -0500

    dep: eslint devient une shared lib

---
+++ packages/eslint-config/react.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import baseConfig from "./index.js";
+
+export default [
+  ...baseConfig,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
